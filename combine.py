5cc45a8ceb23b465b98796c79061ca6b31e92c78
Zaibo Wang <zaibo@fb.com>
2022-06-09 10:53:20 -0700

    combine works on N args

--- combine.py
+++ combine.py
@@ -1,30 +1,36 @@
 import json
 import sys
 
-if len(sys.argv) != 4:
-    exit("Input two data files and output file name")
+output = str(sys.argv[-1])
 
-output = str(sys.argv[3])
+file_list = sys.argv[1:-2]
+if len(file_list) < 1:
+    exit("Need at least 2 input files")
 
-f1 = open(sys.argv[1], 'r')
-f2 = open(sys.argv[2], 'r')
+result = []
+json_list = []
+for file_name in file_list:
+    with open(file_name, 'r') as f:
+        json_list.append(json.load(f))
 
-with open(sys.argv[1]) as f:
-    data1 = json.load(f)
+for i in range(len(json_list) - 1):
+    cur = json_list[i]
+    nxt = json_list[i + 1]
 
-with open(sys.argv[2]) as f:
-    data2 = json.load(f)
+    cur_end_time = cur[-1]["_time"]
+    cur_end_id = cur[-1]["id"]
+    nxt_start_time = nxt[0]["_time"]
 
-data1_end = data1[-1]["_time"]
-data2_start = data2[0]["_time"]
-data1_end_id = data1[-1]["id"]
+    # Include a second of delay when combining
+    delta = nxt_start_time - cur_end_time - 1
 
-# Include a second of delay when combining
-delta = data2_start - data1_end - 1
+    for action in nxt:
+        action["_time"] = action["_time"] - delta
+        action["id"] = action["id"] + cur_end_id
 
-for action in data2:
-    action["_time"] = action["_time"] - delta
-    action["id"] = action["id"] + data1_end_id
+result = []
+for data in json_list:
+    result += data
 
 with open(output, 'w') as f:
-    json.dump(data1 + data2, f, indent=4)
+    json.dump(result, f, indent=4)
