b010d7da2a1b83a1183c91d2ee94b875b6777d33
Zaibo Wang <zaibo@fb.com>
2022-07-04 11:06:12 -0400

    fix combine script

--- combine.py
+++ combine.py
@@ -3,8 +3,8 @@ import sys
 
 output = str(sys.argv[-1])
 
-file_list = sys.argv[1:-2]
-if len(file_list) < 1:
+file_list = sys.argv[1:-1]
+if len(file_list) < 2:
     exit("Need at least 2 input files")
 
 result = []
@@ -17,19 +17,13 @@ for i in range(len(json_list) - 1):
     cur = json_list[i]
     nxt = json_list[i + 1]
 
-    cur_end_time = cur[-1]["_time"]
     cur_end_id = cur[-1]["id"]
-    nxt_start_time = nxt[0]["_time"]
-
-    delta = nxt_start_time - cur_end_time
-
     for action in nxt:
-        action["_time"] = action["_time"] - delta
         action["id"] = action["id"] + cur_end_id
 
-result = []
 for data in json_list:
     result += data
 
+print("Writing combined recording to: " + output)
 with open(output, 'w') as f:
     json.dump(result, f, indent=4)
